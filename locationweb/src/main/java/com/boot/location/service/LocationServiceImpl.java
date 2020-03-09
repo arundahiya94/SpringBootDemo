98a56c874470b99a99ecc71f37386a4299d78fdc
sai <sai@Arun>
2020-03-10 00:48:10 +0530

    Modifed the service layer and used JPARepo instead of CRUDRepo.

--- locationweb/src/main/java/com/boot/location/service/LocationServiceImpl.java
+++ locationweb/src/main/java/com/boot/location/service/LocationServiceImpl.java
@@ -1,39 +1,53 @@
 package com.boot.location.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.boot.location.entities.Location;
+import com.boot.location.repos.LocationRepository;
 
+@Service
 public class LocationServiceImpl implements LocationService {
+
+	@Autowired
+	LocationRepository repository;
 
 	@Override
 	public Location saveLocation(Location location) {
-		// TODO Auto-generated method stub
-		return null;
+		return repository.save(location);
 	}
 
 	@Override
 	public Location updateLocation(Location location) {
-		// TODO Auto-generated method stub
-		return null;
+		return repository.save(location);
 	}
 
 	@Override
 	public void deleteLocation(Location location) {
-		// TODO Auto-generated method stub
-
+		repository.delete(location);
 	}
 
 	@Override
 	public Location getLocationById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Location> loc = repository.findById(id);
+		Location location = loc.get();
+		return location;
 	}
 
 	@Override
 	public List<Location> getAllLocations() {
-		// TODO Auto-generated method stub
-		return null;
+		return repository.findAll();
+	}
+
+	public LocationRepository getRepository() {
+		return repository;
+	}
+
+	public void setRepository(LocationRepository repository) {
+		this.repository = repository;
 	}
 
 }
