a3a8cda1a27394dbff8819093fe3899db1281096
sai <sai@Arun>
2020-03-15 18:05:45 +0530

    Updated the controller and UI to complete the flow form UI to DB.

--- locationweb/src/main/java/com/boot/location/controllers/LocationController.java
+++ locationweb/src/main/java/com/boot/location/controllers/LocationController.java
@@ -1,13 +1,30 @@
 package com.boot.location.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.boot.location.entities.Location;
+import com.boot.location.service.LocationService;
 
 @Controller
 public class LocationController {
 
+	@Autowired
+	LocationService service;
+
 	@RequestMapping("/showCreate")
 	public String showCreate() {
+		return "createLocation";
+	}
+
+	@RequestMapping("/saveLoc")
+	public String saveLocation(@ModelAttribute("location") Location location, ModelMap modelMap) {
+		Location locationSaved = service.saveLocation(location);
+		String msg = "Location saved with Id: " + locationSaved.getId();
+		modelMap.addAttribute("msg", msg);
 		return "createLocation";
 	}
 }
--- locationweb/src/main/webapp/WEB-INF/jsps/createLocation.jsp
+++ locationweb/src/main/webapp/WEB-INF/jsps/createLocation.jsp
@@ -8,17 +8,18 @@
 </head>
 <body>
 
-	<form action="" method="">
-	<pre>
-		Id: <input type="text" name="id" /> 
-		Code: <input type="text" name="code" />
-		Name: <input type="text" name="name" /> 
-		Type: Urban: <input type="radio" name="type" /> 
-			Rural: <input type="radio" name="type" /> 
-		
-		<input type="submit" value="save" />
-	</pre>
-	</form>
+<form action="saveLoc" method="post">
+<pre>
+Id: <input type="text" name="id" /> 
+Code: <input type="text" name="code" />
+Name: <input type="text" name="name" /> 
+Type: Urban: <input type="radio" name="type" value="URBAN" /> 
+	  Rural: <input type="radio" name="type" value="RURAL" /> 
+<input type="submit" value="save" />
+</pre>
+</form>
+
+${msg}
 
 </body>
 </html>
